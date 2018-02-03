9aa4aa4cf2592dc07843cc706b95fccbe99f01a3
rufunt <jazzoo@mail.ru>
2018-02-03 22:32:16 +0300

    Add Complete Tasks action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202191906) do
+ActiveRecord::Schema.define(version: 20180203184217) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180202191906) do
     t.integer "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
